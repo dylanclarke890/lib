06ef9c8f0ee721d5be6ec0dbbac298a076df196d
dylanclarke890 <dylanclarke890@gmail.com>
2023-07-07 04:55:14 +0100

    added j$-cookies

---
+++ j$/j$-cookies.ts
@@ -0,0 +1,33 @@
+const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
+export class Cookies {
+  static set(key: string, value: any, daysToExpire?: number) {
+    const expirationDate = new Date();
+    expirationDate.setTime(expirationDate.getTime() + (daysToExpire ?? 365) * ONE_DAY_IN_MS);
+    document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(
+      value
+    )};expires=${expirationDate.toUTCString()}`;
+  }
+
+  static get(key: string) {
+    const cookie = document.cookie
+      .split("; ") // split into individual cookies
+      .map((cookie) => cookie.split("=").map((c) => decodeURIComponent(c))) // split into decoded name/value pairs
+      .find((cookie) => cookie[0] === key); // look for matching pair
+    return typeof cookie === "undefined" ? undefined : cookie[1];
+  }
+
+  static has(key: string) {
+    return typeof this.get(key) !== "undefined";
+  }
+
+  static remove(key: string) {
+    document.cookie = `${encodeURIComponent(key)}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+  }
+
+  static clear() {
+    document.cookie
+      .split("; ") // split into individual cookies
+      .map((cookie) => decodeURIComponent(cookie.split("=")[0])) // extract the name from each cookie
+      .forEach((key) => this.remove(key));
+  }
+}
