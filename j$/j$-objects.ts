9fe2916660b7f59ed37c0300286b712f06b741f5
dylanclarke890 <dylanclarke890@gmail.com>
2023-07-09 06:34:21 +0100

    set up for J$Objects.recurse

--- j$/j$-objects.ts
+++ j$/j$-objects.ts
@@ -1,1 +1,19 @@
-export class J$Object {}
+export class J$Object {
+  static #recurse(
+    obj: object,
+    callback: (i: number, val: [string, any]) => void,
+    searched: any[]
+  ) {}
+
+  static recurse(obj: object, callback: (i: number, val: [string, any]) => void) {
+    return this.#recurse(obj, callback, []);
+  }
+
+  static recurseKeys(obj: object, callback: (i: number, val: string) => void) {
+    this.recurse(obj, (i, val) => callback(i, val[0]));
+  }
+
+  static recurseValues(obj: object, callback: (i: number, val: any) => void) {
+    this.recurse(obj, (i, val) => callback(i, val[1]));
+  }
+}
