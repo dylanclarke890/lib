cd812c7ea00f8a620c5c5507ab65cce82034f3c3
dylanclarke890 <dylanclarke890@gmail.com>
2023-07-09 05:35:22 +0100

    set up for j$object

--- j$/j$.ts
+++ j$/j$.ts
@@ -1,5 +1,6 @@
 import { J$Cookies } from "./j$-cookies.js";
 import { J$Number } from "./j$-number.js";
+import { J$Object } from "./j$-objects.js";
 import { J$String } from "./j$-string.js";
 import { J$UI } from "./j$-ui.js";
 
@@ -12,4 +13,5 @@ export class J$ {
   static UI = J$UI;
   static String = J$String;
   static Number = J$Number;
+  static Object = J$Object;
 }
